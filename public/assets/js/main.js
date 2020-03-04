e5b54a525cb3d3453f24a32b5929af98d387df6c
Grace <grace@Grace.local>
2020-03-04 20:13:31 +0100

    edit form-designs sass

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,6 +4,15 @@ console.log('>> Ready :)');
 
 
 // Toggle function 
+
+function fill_visibility(id) {
+   const e = document.getElementById('form__design--open');
+   if(e.style.display == 'block')
+      e.style.display = 'none';
+   else
+      e.style.display = 'block';
+}
+
 function fill_visibility(id) {
     const e = document.getElementById('form__fill--open');
     if(e.style.display == 'block')
