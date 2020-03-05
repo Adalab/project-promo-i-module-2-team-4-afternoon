e57fe12474d5fd6f8936075f0b13ea4f7041d487
gavimar <gavimar182@gmail.com>
2020-03-05 20:25:20 +0100

    toggle hidden

---
+++ _src/assets/js/02-toggle_hidden.js
@@ -0,0 +1,30 @@
+
+
+// togle hide-share method
+
+const designShow = document.querySelector('.form__arrow--design');
+const fillShow = document.querySelector('.form__arrow--fill');
+const shareShow = document.querySelector('.form__arrow--share');
+
+function showFillInfo(){
+  const collapsableFill = document.querySelector('#form__fill--open');
+  collapsableFill.classList.toggle('hidden');
+
+}
+
+function showDesignInfo(){
+  const collapsableDesign = document.querySelector('#form__design--open');
+  ccollapsableDesign.classList.toggle('hidden');
+
+}
+
+function showShareInfo(){
+  const collapsableShare = document.querySelector('#form__share--open');
+  collapsableShare.classList.toggle('hidden');
+
+}
+
+
+fillShow.addEventListener('click', showFillInfo);
+designShow.addEventListener('click', showDesignInfo);
+shareShow.addEventListener('click', showShareInfo);
