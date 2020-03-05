6d7cc0f66e6b5561b26be6aa27a9dfd0b4529d1b
Soraya Valle <svalleortega@gmail.com>
2020-03-05 19:35:41 +0100

    Add buttons fuctions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -31,11 +31,27 @@ function fill_visibility(id) {
 
 
 // ------ Button arrow collapse --------
- const btnArrow = document.querySelector('.form__arrow');
+const btnArrowDesign = document.querySelector('.form__arrow--design');
+ btnArrowDesign.addEventListener('click', () => {
+   if (btnArrowDesign.classList.contains('form__arrow--collapse')) {
+      btnArrowDesign.classList.remove('form__arrow--collapse'); } 
+   else { btnArrowDesign.classList.add('form__arrow--collapse'); }
+   });
 
- btnArrow.addEventListener('click', () => {
- if (btnArrow.classList.contains('form__arrow--collapse')) {
-    btnArrow.classList.remove('form__arrow--collapse'); } 
- else { btnArrow.classList.add('form__arrow--collapse'); }
-    }); 
+
+   const btnArrowFill = document.querySelector('.form__arrow--fill');
+ btnArrowFill.addEventListener('click', () => {
+   if (btnArrowFill.classList.contains('form__arrow--collapse')) {
+      btnArrowFill.classList.remove('form__arrow--collapse'); } 
+   else { btnArrowFill.classList.add('form__arrow--collapse'); }
+   });
+
+   const btnArrowShare = document.querySelector('.form__arrow--share');
+ btnArrowShare.addEventListener('click', () => {
+   if (btnArrowShare.classList.contains('form__arrow--collapse')) {
+      btnArrowShare.classList.remove('form__arrow--collapse'); } 
+   else { btnArrowShare.classList.add('form__arrow--collapse'); }
+   });
  // ------ Button arrow collapse --------
+
+ 
