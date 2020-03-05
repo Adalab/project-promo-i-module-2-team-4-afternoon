89c398528ee2f330da9717f7943646be864501a9
beatriz-perez <beatriz.pfp@gmail.com>
2020-03-05 18:22:41 +0100

    add input interactios

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -32,4 +32,20 @@ function fill_visibility(id) {
 //  function btnRotate() {
 //     document.querySelector('form__arow').style.transform = 'rotate(180deg)';
 //   }
+
+// PREVIEW CARD FUNCTIONS **********************************************************+
+
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#job');
+const nameBox = document.querySelector('.nameText');
+const jobBox = document.querySelector('.jobText')
+
+const getText = (x) => {
+   nameBox.innerHTML = nameInput.value;
+   jobBox.innerHTML = jobInput.value;
+}
+
+nameInput.addEventListener('keyup', getText);
+jobInput.addEventListener('keyup', getText);
+
 //# sourceMappingURL=main.js.map
