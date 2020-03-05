4b602cb04183667a699205d06ef1f0227c555ddd
Grace <grace@Grace.local>
2020-03-05 20:51:25 +0100

    test create class

--- _src/assets/js/02-toggle_hidden.js
+++ _src/assets/js/02-toggle_hidden.js
@@ -6,17 +6,19 @@ const designShow = document.querySelector('.form__arrow--design');
 const fillShow = document.querySelector('.form__arrow--fill');
 const shareShow = document.querySelector('.form__arrow--share');
 
+function showDesignInfo(){
+  const collapsableDesign = document.querySelector('#form__design--open');
+  collapsableDesign.classList.toggle('hidden');
+
+}
+
+
 function showFillInfo(){
   const collapsableFill = document.querySelector('#form__fill--open');
   collapsableFill.classList.toggle('hidden');
 
 }
 
-function showDesignInfo(){
-  const collapsableDesign = document.querySelector('#form__design--open');
-  ccollapsableDesign.classList.toggle('hidden');
-
-}
 
 function showShareInfo(){
   const collapsableShare = document.querySelector('#form__share--open');
