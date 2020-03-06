74a87029a171020d1890c2ace1f344a7ae6bb44b
beatriz-perez <beatriz.pfp@gmail.com>
2020-03-06 11:25:33 +0100

    get buttons working

--- _src/assets/js/cardPreview.js
+++ _src/assets/js/cardPreview.js
@@ -18,23 +18,65 @@ jobInput.addEventListener('keyup', getText);
 // Social Buttons - MAIL ---------------------------------------------------------------------
 const emailBox = document.querySelector('#email');
 const emailButton = document.querySelector('#emailButton');
+
 const showEmailButton = () => emailBox.value.length !== 0 ? emailButton.classList.remove('hidden') : emailButton.classList.add('hidden');
-emailBox.addEventListener('keyup', showEmailButton);
+const getEmailLink = () => { emailButton.querySelector('#emailButtonLink').href = `mailto:${emailBox.value}` };
+
+const emailHandler = () => {
+   showEmailButton();
+   getEmailLink();
+}
+
+emailBox.addEventListener('keyup', emailHandler);
 
 // Social Buttons - PHONE ---------------------------------------------------------------------
 const phoneBox = document.querySelector('#phone');
 const phoneButton = document.querySelector('#phoneButton');
+
 const showPhoneButton = () => phoneBox.value.length !== 0 ? phoneButton.classList.remove('hidden') : phoneButton.classList.add('hidden');
-phoneBox.addEventListener('keyup', showPhoneButton);
+const getPhoneLink = () => { phoneButton.querySelector('#phoneButtonLink').href = `tel:${phoneBox.value}` };
+
+const phoneHandler = () => {
+   showPhoneButton();
+   getPhoneLink();
+}
+
+phoneBox.addEventListener('keyup', phoneHandler);
 
 // Social Buttons - LINKEDIN ---------------------------------------------------------------------
 const linkedinBox = document.querySelector('#linkedin');
 const linkedinButton = document.querySelector('#linkedinButton');
+
 const showLinkedinButton =  () => linkedinBox.value.length !== 0 ? linkedinButton.classList.remove('hidden') : linkedinButton.classList.add('hidden');
-linkedinBox.addEventListener('keyup', showLinkedinButton);
+const getLinkedinLink = () => { linkedinButton.querySelector('#linkedinButtonLink').href = `https://www.linkedin.com/in/${linkedinBox.value}` };
+
+const linkedinHandler = () => {
+   showLinkedinButton();
+   getLinkedinLink();
+}
+
+linkedinBox.addEventListener('keyup', linkedinHandler);
 
 // Social Buttons - GITHUB ---------------------------------------------------------------------
 const githubBox = document.querySelector('#github');
 const githubButton = document.querySelector('#githubButton');
+
 const showGithubButton =  () => githubBox.value.length !== 0 ? githubButton.classList.remove('hidden') : githubButton.classList.add('hidden');
-githubBox.addEventListener('keyup', showGithubButton);
+const getGithubLink = () => { githubButton.querySelector('#githubButtonLink').href = `https://www.github.com/${githubBox.value}` };
+
+const githubHandler = () => {
+   showGithubButton();
+   getGithubLink();
+}
+
+
+githubBox.addEventListener('keyup', githubHandler);
+
+// SHARE - create and share buttons ----------------------------------------------------------------
+
+const createCardButton = document.querySelector('#createCardButton');
+const createCard = () => {
+   event.preventDefault();
+   console.log('a new card has been created'); // AÑADIR EN ESTA FUNCIÓN acciones para crear tarjeta
+}
+createCardButton.addEventListener('click', createCard);
