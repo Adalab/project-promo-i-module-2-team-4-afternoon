6369e39bbd0af5fb9f60bac0e91fba48b3f3a3a0
Soraya Valle <svalleortega@gmail.com>
2020-03-06 18:31:38 +0100

    Shorten arrow collapse code

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,31 +1,27 @@
-<<<<<<< HEAD
 'use strict';
 
-console.log('>> Ready :)');
-
-// ------ Button arrow collapse --------
+// Button arrow collapse design
 const btnArrowDesign = document.querySelector('.form__arrow--design');
- btnArrowDesign.addEventListener('click', () => {
-   if (btnArrowDesign.classList.contains('form__arrow--collapse')) {
-      btnArrowDesign.classList.remove('form__arrow--collapse'); } 
-   else { btnArrowDesign.classList.add('form__arrow--collapse'); }
-   });
+function btnToggleDesign() {
+  btnArrowDesign.classList.toggle('form__arrow--collapse');
+}
+btnArrowDesign.addEventListener('click', btnToggleDesign);
 
+// Button arrow collapse fill
 
-   const btnArrowFill = document.querySelector('.form__arrow--fill');
- btnArrowFill.addEventListener('click', () => {
-   if (btnArrowFill.classList.contains('form__arrow--collapse')) {
-      btnArrowFill.classList.remove('form__arrow--collapse'); } 
-   else { btnArrowFill.classList.add('form__arrow--collapse'); }
-   });
+const btnArrowFill = document.querySelector('.form__arrow--fill');
+function btnToggleFill() {
+  btnArrowFill.classList.toggle('form__arrow--collapse');
+}
+btnArrowFill.addEventListener('click', btnToggleFill);
 
-   const btnArrowShare = document.querySelector('.form__arrow--share');
- btnArrowShare.addEventListener('click', () => {
-   if (btnArrowShare.classList.contains('form__arrow--collapse')) {
-      btnArrowShare.classList.remove('form__arrow--collapse'); } 
-   else { btnArrowShare.classList.add('form__arrow--collapse'); }
-   });
- // ------ Button arrow collapse --------
+// Button arrow collapse share
+
+const btnArrowShare = document.querySelector('.form__arrow--share');
+function btnToggleShare() {
+  btnArrowShare.classList.toggle('form__arrow--collapse');
+}
+btnArrowShare.addEventListener('click', btnToggleShare);
 
 
 // togle hide-share method
@@ -56,8 +52,6 @@ function showShareInfo(){
 fillShow.addEventListener('click', showFillInfo);
 designShow.addEventListener('click', showDesignInfo);
 shareShow.addEventListener('click', showShareInfo);
-//# sourceMappingURL=main.js.map
-=======
 // PREVIEW CARD FUNCTIONS **********************************************************+
 
 // Text info --------------------------------------------------------------------------
@@ -140,41 +134,4 @@ const createCard = () => {
    console.log('a new card has been created'); // AÑADIR EN ESTA FUNCIÓN acciones para crear tarjeta
 }
 createCardButton.addEventListener('click', createCard);
-'use strict';
-
-console.log('>> Ready :)');
-
-
-// Toggle function 
-
-function fill_visibility(id) {
-   const e = document.getElementById('form__design--open');
-   if(e.style.display == 'block')
-      e.style.display = 'none';
-   else
-      e.style.display = 'block';
-}
-
-function fill_visibility(id) {
-    const e = document.getElementById('form__fill--open');
-    if(e.style.display == 'block')
-       e.style.display = 'none';
-    else
-       e.style.display = 'block';
- }
-
- function share_visibility(id) {
-    const e = document.getElementById('form__share--open');
-    if(e.style.display == 'block')
-       e.style.display = 'none';
-    else
-       e.style.display = 'block';
- }
-
-//  function btnRotate() {
-//     document.querySelector('form__arow').style.transform = 'rotate(180deg)';
-//   }
-
-
 //# sourceMappingURL=main.js.map
->>>>>>> cardPreview
