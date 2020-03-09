b209658b7e9f3d32ba89e7cbbdf2352202c45520
Grace <grace@Grace.local>
2020-03-09 20:47:37 +0100

    add new palette

--- _src/assets/js/04-design-preview.js
+++ _src/assets/js/04-design-preview.js
@@ -4,6 +4,7 @@
 const palette1 = document.querySelector('#paletteOne');
 const palette2 = document.querySelector('#paletteTwo');
 const palette3 = document.querySelector('#paletteThree');
+const palette4 = document.querySelector('#paletteFour');
 const palettePreview = document.querySelector('#changePreviewStyle');
 //using for of loop
 // const socialButton = document.querySelectorAll('.preview__buttonList--item')
@@ -11,6 +12,7 @@ const palettePreview = document.querySelector('#changePreviewStyle');
 function selectPalette1(){
     palettePreview.classList.add('infoStyle1');
     palettePreview.classList.remove('infoStyle2');palettePreview.classList.remove('infoStyle3');
+    palettePreview.classList.remove('infoStyle4');
 
     // for (let preview of socialButton) {
     //  socialButton.classList.add('')<--create style
@@ -22,6 +24,7 @@ function selectPalette2(){
     palettePreview.classList.remove('infoStyle1');
     palettePreview.classList.add('infoStyle2');
     palettePreview.classList.remove('infoStyle3');
+    palettePreview.classList.remove('infoStyle4');
     
     console.log('not working!!');
 }
@@ -30,6 +33,17 @@ function selectPalette3(){
     palettePreview.classList.remove('infoStyle1');
     palettePreview.classList.remove('infoStyle2');
     palettePreview.classList.add('infoStyle3');
+    palettePreview.classList.remove('infoStyle4');
+
+    console.log('fffff....');
+}
+
+
+function selectPalette4(){
+    palettePreview.classList.remove('infoStyle1');
+    palettePreview.classList.remove('infoStyle2');
+    palettePreview.classList.remove('infoStyle3');
+    palettePreview.classList.add('infoStyle4');
 
     console.log('fffff....');
 }
@@ -38,3 +52,4 @@ function selectPalette3(){
 palette1.addEventListener('click', selectPalette1);
 palette2.addEventListener('click', selectPalette2);
 palette3.addEventListener('click', selectPalette3);
+palette4.addEventListener('click', selectPalette4);
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -183,6 +183,7 @@ createCardButton.addEventListener('click', createCard);
 const palette1 = document.querySelector('#paletteOne');
 const palette2 = document.querySelector('#paletteTwo');
 const palette3 = document.querySelector('#paletteThree');
+const palette4 = document.querySelector('#paletteFour');
 const palettePreview = document.querySelector('#changePreviewStyle');
 //using for of loop
 // const socialButton = document.querySelectorAll('.preview__buttonList--item')
@@ -190,6 +191,7 @@ const palettePreview = document.querySelector('#changePreviewStyle');
 function selectPalette1(){
     palettePreview.classList.add('infoStyle1');
     palettePreview.classList.remove('infoStyle2');palettePreview.classList.remove('infoStyle3');
+    palettePreview.classList.remove('infoStyle4');
 
     // for (let preview of socialButton) {
     //  socialButton.classList.add('')<--create style
@@ -201,6 +203,7 @@ function selectPalette2(){
     palettePreview.classList.remove('infoStyle1');
     palettePreview.classList.add('infoStyle2');
     palettePreview.classList.remove('infoStyle3');
+    palettePreview.classList.remove('infoStyle4');
     
     console.log('not working!!');
 }
@@ -209,6 +212,17 @@ function selectPalette3(){
     palettePreview.classList.remove('infoStyle1');
     palettePreview.classList.remove('infoStyle2');
     palettePreview.classList.add('infoStyle3');
+    palettePreview.classList.remove('infoStyle4');
+
+    console.log('fffff....');
+}
+
+
+function selectPalette4(){
+    palettePreview.classList.remove('infoStyle1');
+    palettePreview.classList.remove('infoStyle2');
+    palettePreview.classList.remove('infoStyle3');
+    palettePreview.classList.add('infoStyle4');
 
     console.log('fffff....');
 }
@@ -217,4 +231,5 @@ function selectPalette3(){
 palette1.addEventListener('click', selectPalette1);
 palette2.addEventListener('click', selectPalette2);
 palette3.addEventListener('click', selectPalette3);
+palette4.addEventListener('click', selectPalette4);
 //# sourceMappingURL=main.js.map
