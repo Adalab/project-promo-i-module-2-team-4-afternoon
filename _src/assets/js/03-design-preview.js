154984fe8363e5fe2a49c7e8603c2678dc498fb1
gavimar <gavimar182@gmail.com>
2020-03-18 20:07:31 +0100

    storageLocalPalette

--- _src/assets/js/03-design-preview.js
+++ _src/assets/js/03-design-preview.js
@@ -21,6 +21,7 @@ function selectPalette1() {
     button.classList.remove('buttonStyle4');
   }
   console.log('hey');
+  saveLocalPalette();
 }
 
 function selectPalette2() {
@@ -36,6 +37,7 @@ function selectPalette2() {
     button.classList.remove('buttonStyle4');
   }
   console.log('not working!!');
+  saveLocalPalette();
 }
 
 function selectPalette3() {
@@ -51,6 +53,7 @@ function selectPalette3() {
     button.classList.remove('buttonStyle4');
   }
   console.log('fffff....');
+  saveLocalPalette();
 }
 
 function selectPalette4() {
@@ -66,9 +69,103 @@ function selectPalette4() {
     button.classList.add('buttonStyle4');
   }
   console.log('fffff....');
+  saveLocalPalette();
 }
 
 palette1.addEventListener('click', selectPalette1);
 palette2.addEventListener('click', selectPalette2);
 palette3.addEventListener('click', selectPalette3);
 palette4.addEventListener('click', selectPalette4);
+
+
+// localstorage
+
+
+
+function saveLocalPalette(){
+  
+  if (palette1.checked){
+    localStorage.setItem('theme',palette1.value);
+  }
+
+  if (palette2.checked){
+    localStorage.setItem('theme',palette2.value);
+  }
+
+  if (palette3.checked){
+    localStorage.setItem('theme',palette3.value);
+  }
+
+  if (palette4.checked){
+    localStorage.setItem('theme',palette4.value);
+  }
+}
+
+function getTheme(){
+  if (localStorage.getItem('theme') === palette1.value){
+  
+    palettePreview.classList.add('infoStyle1');
+  palettePreview.classList.remove('infoStyle2');
+  palettePreview.classList.remove('infoStyle3');
+  palettePreview.classList.remove('infoStyle4');
+
+  for (let button of socialButtons) {
+    button.classList.add('buttonStyle1');
+    button.classList.remove('buttonStyle2');
+    button.classList.remove('buttonStyle3');
+    button.classList.remove('buttonStyle4');
+  }
+    palette1.checked = true;
+  }
+
+  if (localStorage.getItem('theme') === palette2.value){
+    palettePreview.classList.remove('infoStyle1');
+  palettePreview.classList.add('infoStyle2');
+  palettePreview.classList.remove('infoStyle3');
+  palettePreview.classList.remove('infoStyle4');
+
+  for (let button of socialButtons) {
+    button.classList.remove('buttonStyle1');
+    button.classList.add('buttonStyle2');
+    button.classList.remove('buttonStyle3');
+    button.classList.remove('buttonStyle4');
+  }
+    palette2.checked = true;
+  }
+
+  if (localStorage.getItem('theme') === palette3.value){
+    palettePreview.classList.remove('infoStyle1');
+  palettePreview.classList.remove('infoStyle2');
+  palettePreview.classList.add('infoStyle3');
+  palettePreview.classList.remove('infoStyle4');
+
+  for (let button of socialButtons) {
+    button.classList.remove('buttonStyle1');
+    button.classList.remove('buttonStyle2');
+    button.classList.add('buttonStyle3');
+    button.classList.remove('buttonStyle4');
+  }
+    palette3.checked = true;
+
+}
+
+if (localStorage.getItem('theme') === palette4.value){
+  palettePreview.classList.remove('infoStyle1');
+  palettePreview.classList.remove('infoStyle2');
+  palettePreview.classList.remove('infoStyle3');
+  palettePreview.classList.add('infoStyle4');
+
+  for (let button of socialButtons) {
+    button.classList.remove('buttonStyle1');
+    button.classList.remove('buttonStyle2');
+    button.classList.remove('buttonStyle3');
+    button.classList.add('buttonStyle4');
+  }
+  palette4.checked = true;
+
+}
+
+palette1.addEventListener('click', getTheme);
+palette2.addEventListener('click', getTheme);
+palette3.addEventListener('click', getTheme);
+palette4.addEventListener('click', getTheme);
